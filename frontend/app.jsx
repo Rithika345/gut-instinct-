189d83b97c483a47df5928a96be613d68f46ce2f
rithika345 <rithikakrishna1@gmail.com>
2026-05-14 02:00:12 -0700

    Increase timeout to 15 min

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -153,7 +153,7 @@ function Demo() {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(caseObj.request),
           }),
-          new Promise((_, rej) => setTimeout(() => rej(new Error('timeout')), 900000)),
+          new Promise((_, rej) => setTimeout(() => rej(new Error('timeout')), 9000000)),
         ]);
         if (res.ok) { apiResult = await res.json(); setIsLive(true); }
       } catch {}
